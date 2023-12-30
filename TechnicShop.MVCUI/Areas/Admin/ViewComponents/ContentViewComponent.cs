2a66f64359379947552b6287351a51e02f9545f7
Ahmet <aeldra995@hotmail.com>
2023-12-30 13:31:52 +0300

    #Layout

---
+++ TechnicShop.MVCUI/Areas/Admin/ViewComponents/ContentViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechnicShop.MVCUI.Areas.Admin.ViewComponents
+{
+    public class ContentViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ TechnicShop.MVCUI/Areas/Admin/ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechnicShop.MVCUI.Areas.Admin.ViewComponents
+{
+    public class SideBarViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
